f7658bfa1b33e37d04f55dc445549d7e8bffb436
ayaz-alijamali <ayazalijamali64@gmail.com>
2024-05-02 15:49:57 +0500

    100 days coding challenge Question

---
+++ mergeText.js
@@ -0,0 +1,32 @@
+// Question ____________ 64
+
+
+function mergeTextWithNumber(text, number) {
+    return text + number;
+}
+
+// Example usage
+console.log(mergeTextWithNumber("Age: ", 30));  // Output: Age: 30
+
+
+// Question ___________ 65
+
+
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+
+// Example usage
+console.log(remainder(5, 2));  // Output: 1
+
+
+// Question ____________ 66
+
+
+function checkBothTrue(value1, value2) {
+    return value1 && value2;
+}
+
+// Example usage
+console.log(checkBothTrue(true, false));  // Output: false
+console.log(checkBothTrue(true, true));   // Output: true
